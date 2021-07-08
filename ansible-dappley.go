12cb1c49fc10185ead54e3ac3bee5c3a2ad93044
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-07-08 15:50:36 -0700

    Update ansible-dappley.go

--- ansible-dappley.go
+++ ansible-dappley.go
@@ -164,22 +164,48 @@ func initialize(number string) {
 //Termiante all servers via aws cli command
 func terminate(number string) {
 	var updated_instance_list string
-	fileName := "instance_ids"
+	var updated_host_list string
+	fileName_1 := "instance_ids"
+	fileName_2 := "hosts"
+
 
 	to_terminate, err := strconv.Atoi(number)
 	if err != nil {
 		panic(err)
 	}
 
-	instance_byte, err := ioutil.ReadFile(fileName)
+	lines_to_remove := to_terminate * 2
+
+	hosts_byte, err := ioutil.ReadFile(fileName_2)
 	if err != nil {
-		fmt.Println("Failed to read", fileName, "!")
+		fmt.Println("Failed to read", fileName_2, "!")
 		return
 	}
+	host_scanner := bufio.NewScanner(strings.NewReader(string(hosts_byte)))
+	for host_scanner.Scan() {
+		line := host_scanner.Text()
 
-	scanner := bufio.NewScanner(strings.NewReader(string(instance_byte)))
-	for scanner.Scan() {
-		instance_id := scanner.Text()
+		fmt.Println(line)
+		if lines_to_remove == 0 {
+			updated_host_list += line + "\n"
+			continue
+		}
+
+		lines_to_remove -= 1
+	}
+	err = ioutil.WriteFile(fileName_2, []byte(updated_host_list), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	instance_byte, err := ioutil.ReadFile(fileName_1)
+	if err != nil {
+		fmt.Println("Failed to read", fileName_1, "!")
+		return
+	}
+	instance_scanner := bufio.NewScanner(strings.NewReader(string(instance_byte)))
+	for instance_scanner.Scan() {
+		instance_id := instance_scanner.Text()
 		if to_terminate == 0 {
 			updated_instance_list += instance_id + "\n"
 			continue
@@ -197,7 +223,7 @@ func terminate(number string) {
 		to_terminate -= 1
 	}
 
-	err = ioutil.WriteFile(fileName, []byte(updated_instance_list), 0644)
+	err = ioutil.WriteFile(fileName_1, []byte(updated_instance_list), 0644)
 	if err != nil {
 		log.Fatalln(err)
 	}
