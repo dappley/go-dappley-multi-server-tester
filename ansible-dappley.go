6396b741d67d31abb10557d174376c3bd6d4508f
Heesoo Hwang <heesoohwang1231@gmail.com>
2021-07-30 09:35:17 -0700

    Update ansible-dappley.go

--- ansible-dappley.go
+++ ansible-dappley.go
@@ -279,9 +279,9 @@ func allFiles(directory string) []string {
     if err != nil {
         panic(err)
     }
-    for _, file := range files {
-        fmt.Println(file)
-	}
+    // for _, file := range files {
+    //     fmt.Println(file)
+	// }
 	
 	return files
 }
